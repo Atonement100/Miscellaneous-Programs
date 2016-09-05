6621df725ed46e808557a408ef47e534b5c6d192
Timothy Russell-Wagner <atonement100@gmail.com>
2016-09-05 14:26:41 -0400

    Baseline functionality of prog2

--- Cryptology/Exercise 2 - Block Transposition/prog2_2.cpp
+++ Cryptology/Exercise 2 - Block Transposition/prog2_2.cpp
@@ -31,7 +31,130 @@
 
 #include "prog2_2.h"
 
+
 int main(int argc, char * argv[])
 {
+	if (ProcessArgs(argc, argv) != 0) return -1;
+	std::ifstream KeyFile;
+	KeyFile.open(FilenameToOpen.c_str());
+	if (!KeyFile.is_open()) {
+		std::cerr << "Error opening file with filename " << FilenameToOpen << ". Exiting." << std::endl;
+		return -1;
+	}
 
+	std::vector<int> Key;
+	if (ProcessInput(Key) != 0) return -1;
+
+
+	std::cout << "Should encrypt: " << ShouldEncrypt << "\nBlock Length: " << PadLength << "\nFile opened: " << FilenameToOpen << std::endl;
+
+	return 0;
+}
+
+int ProcessInput() {
+	
+	return 0;
+}
+
+int ProcessKeyFile(std::ifstream& KeyFile, std::vector<int> &Key)
+{
+	std::string InputStr = "";
+	char Input;
+	while (KeyFile.get(Input)) { InputStr += Input; }
+
+	if (InputStr.length() != BlockSize) {
+		std::cerr << "Either a malformed binary key file has been passed, or the -x flag should have been used (i.e. for a hex key file)." << std::endl;
+		return -1;
+	}
+
+	if (CheckBinaryString(InputStr, Key) != 0) return -1
+
+	return 0;
+	
+}
+
+int CheckBinaryString(std::string InputStr, std::vector<int> &Key) {
+	std::vector<bool> NumUsed(InputStr.length());
+	for (int Index = 0; Index < NumUsed.size(); Index++) {
+		NumUsed[Index] = false;
+	}
+
+	for (int Index = 0; Index < InputStr.length(); Index++) {
+		int DecChar = (int)InputStr[Index];
+		if (DecChar < 0) DecChar += 256;
+
+	}
+}
+
+int ProcessInput(std::vector<int> &Key) {
+
+	return 0;
+}
+
+
+
+int ProcessArgs(int argc, char* argv[]) {
+	if (argc < 3 || argc > 4) {
+		std::cerr << "Invalid number of arguments passed. Usage is [-e|-d] (-b<n>) -k<filename>." << std::endl;
+		return -1;
+	}
+	else {
+		std::string* Arguments = new std::string[argc - 1];
+		for (int Args = 1; Args < argc; Args++) {
+			Arguments[Args - 1] = std::string(argv[Args]);
+			if (CheckArg(Arguments[Args - 1], Args, argc) == 0) continue;
+			else {
+				return -1;
+			}
+		}
+	}
+
+	return 0;
+}
+
+int CheckArg(std::string InArg, int ArgNum, int ArgCount) {
+	switch (ArgNum) {
+	case 1:
+		if (InArg == EncryptIn) {
+			ShouldEncrypt = true;
+		}
+		else if (InArg == DecryptIn) {
+			ShouldEncrypt = false;
+		}
+		else {
+			std::cerr << "First parameter invalid. Should be [-e|-d]." << std::endl;
+			return -1;
+		}
+		return 0;
+	case 2:
+		if (ArgCount == 4) {
+			if (InArg.substr(0, 2) == BlockArg) {
+				BlockSize = atoi((InArg.substr(2)).c_str());
+				if (BlockSize < 1) {
+					std::cerr << "Invalid pad length. Must be greater than zero." << std::endl;
+					return -1;
+				}
+			}
+			else {
+				std::cerr << "Second parameter invalid. Should be -b<n>." << std::endl;
+				return -1;
+			}
+		}
+		//No return here, if arg count is less than 4, then we want to fallthrough to case 3. i.e. usage would be [-e|-d] -k<filename> in this case
+	case 3:
+		if (InArg.substr(0, 2) == FileIn) {
+			if (InArg.substr(0, 2) == FileIn) {
+				FilenameToOpen = InArg.substr(2);
+			}
+			else {
+				std::cerr << "Second parameter invalid. Should be -k<filename>." << std::endl;
+				return -1;
+			}
+			return 0;
+		}
+		return 0;
+	default:
+		std::cerr << "Invalid number of parameters" << std::endl;
+		return -1;
+	}
 }
--- Cryptology/Exercise 2 - Block Transposition/prog2_2.h
+++ Cryptology/Exercise 2 - Block Transposition/prog2_2.h
@@ -2,6 +2,23 @@
 #include <iostream>
 #include <fstream>
 #include <cstdio>
+#include <cstdlib>
 #include <vector>
 #include <string>
 #endif // Guard
+
+const std::string EncryptIn = "-e";
+const std::string DecryptIn = "-d";
+const std::string BlockArg = "-b";
+const std::string FileIn = "-k";
+
+bool ShouldEncrypt = false;
+int BlockSize = 8;
+std::string FilenameToOpen;
+
+int InvertKey(std::vector<int> &Key);
+int ProcessArgs(int argc, char* argv[]);
+int CheckArg(std::string InArg, int ArgNum, int ArgCount);
+int ProcessInput(std::vector<int> &Key);
+int ProcessKeyFile(std::ifstream& KeyFile, std::vector<int> &Key);
+int CheckBinaryString(std::string Input, std::vector<int> &Key);
