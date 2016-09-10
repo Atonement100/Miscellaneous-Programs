c723744033b9d8405c89f1150f8e2e46cb0c7490
Timothy Russell-Wagner <atonement100@gmail.com>
2016-09-10 14:26:18 -0400

    Update explanation to what I ended up writing as my solution

--- PairGenerator.cpp
+++ PairGenerator.cpp
@@ -15,9 +15,20 @@ that more than n/2 of the chips are good. Show that floor(n/2) pairwise
 tests are sufficient to reduce the problem to one of nearly half the size.
 
 I used this to generate a small sample of pairs that should follow the specification, and then used
-those pairs to test solutions. The one I settled on as of this writing rotates the first item of each pair
-to the next pair (final pair loops to the first) after each iteration, discarding any pairs that do not
-identify each other as good.
+those pairs to test solutions. The solution that I've written for this is as follows.
+
+For each pair in p, if the chips do not identify each other as good, then at least one is bad, 
+so that pair should be discarded. For the remaining pairs in p, take the first chip of each pair and 
+apply this method to pairs formed in the new group p. 
+
+This will recursively eliminate all pairs that do not identify each other as good. 
+When the number of items in the subset p after verifying that they identify each other 
+as good is 1 or 2 (i.e. both identify as good, or the group is thrown away and 
+the leftover chip that was not in a group), then those chips are good, since there are 
+more good chips than bad chips. 
+
+Note that this program only generates and eliminates the first level of chips. It does not carry out
+the completed algorithm, though it would not be hard to implement from here.
 */
 
 #include <iostream>
