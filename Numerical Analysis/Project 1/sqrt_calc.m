4779205583526cc5848e376ce7970a2d895862f5
Timothy Russell-Wagner <t.wagner@ufl.edu>
2016-10-14 20:23:43 -0400

    sqrt calc now provides a rough estimate of sqrt to accelerate convergence

--- Numerical Analysis/Project 1/sqrt_calc.m
+++ Numerical Analysis/Project 1/sqrt_calc.m
@@ -1,20 +1,30 @@
-function root = sqrt_calc( sq )
+function x = sqrt_calc( sq )
     steps = 0;
     fsq = sq;
-    x = sq;
+    
+    %Find start location, could be done significantly faster
+    %with the use of log() to get exponent; only allowed +-*/
+    exp = 0;
+    tempsq = sq;
+    while (abs(tempsq) > 10^-1)
+        tempsq = tempsq / 10;
+        exp = exp + 1;
+    end
+    exp = round((exp-1)/2);
+    
+    x = 10^exp;
+    
     while (abs(fsq) > 10^-7 && steps < 100)
        fsq = sq - x^2;
        dfsq = -2*x;
        x = x - (fsq)/(dfsq);
-
        steps = steps + 1;    
-       root = x;
     end
     
     if abs(fsq) > 10^-7
         disp('Warning: A root may not have been found')
     else 
-        fprintf('A root has been found in %d steps\nThe square root of %d is %f\n',steps,sq,root)
+        fprintf('A root has been found in %d steps\nThe square root of %d is %f\n',steps,sq,x)
     end
 end
 
